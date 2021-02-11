5536b81082f96aa780635a8f403c12ee212149b9
lukas <lukasz.grazda@gmail.com>
2021-02-11 13:12:16 +0100

    first commit

---
+++ src/test/java/movierental/com/ApplicationTests.java
@@ -0,0 +1,13 @@
+package movierental.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
